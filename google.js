23b5e6d636d75e08275888181d0b584b88fa1609
David Garcia Gonzalez <dgarci23@nd.edu>
2021-03-07 01:47:27 -0500

    Start GoogleSheets Integration

--- google.js
+++ google.js
@@ -1,5 +1,25 @@
-const {google} = require('googleapis');
+const fs = require('fs');
+const readline = require('readline');
+const {GoogleSpreadsheet} = require("google-spreadsheet");
+const {promisify} = require("util");
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const creds = require("./creds.json");
 
-const TOKEN_PATH = 'token.json';
+async function accessSpreadsheet() {
+    const doc = new GoogleSpreadsheet("1by1-9FXUU7Rd1YwdgW5CtmLvtnMrVsEN45IAuYAjlEU");
+
+    await doc.useServiceAccountAuth({
+        client_email: creds.client_email,
+        private_key: creds.private_key
+    });
+
+
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByIndex[0];
+
+    const data = sheet.getCell(0,0);
+    console.log(data);
+}
+
+accessSpreadsheet();
